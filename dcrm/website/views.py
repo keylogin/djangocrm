07d47affdee4197fc604469feb579ff7521c0a35
keylogin <seriousam_007@yahoo.com>
2025-02-13 12:04:21 +0200

    records

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
 # Create your views here.
-
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
     # Check to see if loggin in
     if request.method == "POST":
         username = request.POST['username']
@@ -21,7 +22,7 @@ def home(request):
             messages.success(request, "There Was An Error Logging in, Please Try Again....")
             return redirect('home')
     else:    
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
@@ -47,3 +48,13 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
 
     return render(request, 'register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You Must Be Logged In First...")
+        return redirect('home')
